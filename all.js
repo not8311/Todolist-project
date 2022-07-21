a7e7dbb4b4696b16dc7f71c86ceb8363136953a9
not8311 <yichunlin09@gmail.com>
2022-07-21 13:15:27 +0800

    增加ID

--- all.js
+++ all.js
@@ -1,42 +1,7 @@
-let data = [
-    {
-        content: "把冰箱發,霉的檸檬拿去丟",
-        "state": "待完成",
-        "check": ""
-    },
-    {
-        content: "打電話叫媽媽匯款給我",
-        "state": "已完成",
-        "check":"checked"
-    },
-    {
-        content: "整理電腦資料夾",
-        "state": "待完成",
-        "check": ""
-    },
-    {
-        content: "繳電費水費瓦斯費",
-        "state": "已完成",
-        "check":"checked"
-    },
-    {
-        content: "刪訊息",
-        "state": "待完成",
-        "check": ""
-    },
-    {
-        content: "約vicky禮拜三泡溫泉",
-        "state": "待完成",
-        "check": ""
-    },
-    {
-        content: "約ada禮拜四吃晚餐",
-        "state": "待完成",
-        "check": ""
-    }
-];
+let data = [];
 
 const cardInput = document.querySelector(".input");
+const addBtn = document.querySelector(".btn_add");
 const list = document.querySelector(".list");
 const txt = document.querySelector(".txt");
 const tab = document.querySelector(".tab");
@@ -47,11 +12,21 @@ const delAll = document.querySelector(".delAll");
 
 // 渲染功能
 function render(){
+    let unchecked = data.filter((item)=>{
+        if(tab.querySelector(".active").textContent === "已完成"){
+            return item.check == "checked";
+        }else if(tab.querySelector(".active").textContent === "待完成"){
+            return item.check == "";
+        }else{
+            return item.check == "checked" || item.check == "";
+        }
+    })
     let src = "";
-    data.forEach((item,index) => src += `<li><label class="checkbox" for=""><input type="checkbox" data-check="${item.state}" data-num = ${index} ${item.check}/><span>${item.content}</span></label><a href="#" class="delete" data-num = ${index}></a>
+    unchecked.forEach((item,index) => src += `<li data-id =${item.id}><label class="checkbox" for=""><input type="checkbox" data-check="${item.state}" data-num = ${index} ${item.check}/><span>${item.content}</span></label><a href="#" class="delete" data-num = ${index}></a>
     </li>`);
 
     list.innerHTML = src;
+
     // 顯示待完成數
     let total = 0;
     data.forEach((item)=>{
@@ -62,25 +37,31 @@ function render(){
     uncompleted.textContent = `${total} 個待完成項目`;
 }
 
-render();
-
-// 新增
-cardInput.addEventListener("click",(e) => {
-    if(e.target.nodeName !== "A"){
-        return;
+// 按鈕新增
+addBtn.addEventListener("click",addItem)
+// 鍵盤監聽
+txt.addEventListener("keypress",function(e){
+    if(e.key === "Enter"){
+        addItem();
     }
-    if(txt.value.trim() == ""){
-        alert("請輸入文字");
-        return;
-    }
-    let obj ={};
-    obj.content = txt.value;
-    obj.state = "待完成";
-    obj.check = "";
-    data.push(obj);
-    render();
-    txt.value = "";
 })
+// 新增
+function addItem(e){
+    let id = new Date().getTime();
+    let obj ={};
+        obj.content = txt.value;
+        obj.state = "待完成";
+        obj.check = "";
+        obj.id = id;
+    if(txt.value.trim() !== ""){
+        data.push(obj);
+        render();
+        txt.value = "";
+    }else{
+        alert("請輸入文字");
+    }
+    
+}
 
 // 分類
 tab.addEventListener("click",(e) =>{
@@ -89,19 +70,7 @@ tab.addEventListener("click",(e) =>{
     tabs.forEach((item)=>item.setAttribute("class",""));
     // 被點選加上class
     e.target.setAttribute("class","active");
-    
-    let str = "";
-    if(e.target.textContent === "全部"){
-        render();
-        return;
-    }
-    data.forEach((item,index)=>{
-        if(item.state === e.target.textContent){
-            str += `<li><label class="checkbox" for=""><input type="checkbox" data-check="${item.state}" data-num = ${index} ${item.check} /><span>${item.content}</span></label><a href="#" class="delete" data-num = ${index}></a>
-            </li>`;
-        }
-    })
-    list.innerHTML = str;
+    render();
 })
 
 list.addEventListener("click",(e) => {
@@ -109,17 +78,22 @@ list.addEventListener("click",(e) => {
     if(e.target.nodeName === "A"){
         let num = e.target.getAttribute("data-num");
         data.splice(num,1);
-        render();
+        render(data);
     }
     // 更改state
-    else if(e.target.nodeName === "INPUT"){
-        let obj ={};
-        obj.check = e.target.check = "checked";
-        obj.state = "已完成"
-        data[e.target.getAttribute("data-num")].check = obj.check;
-        data[e.target.getAttribute("data-num")].state = obj.state;
-        render();
-    }
+    let id = parseInt(e.target.closest("li").dataset.id);
+    data.forEach((item)=>{
+        if(item.id === id){
+            if(item.check === "checked"){
+                item.check = "";
+                item.state = "待完成";
+            }else{
+                item.check ="checked";
+                item.state = "已完成";
+            }
+        }
+    })    
+    render();
 })
 
 // 清除已完成項目
@@ -127,3 +101,5 @@ delAll.addEventListener("click",(e)=>{
     data = data.filter((item)=> item.state === "待完成");
     render();
 })
+
+render();
